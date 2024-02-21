34462dd43d154a90ee3d649c656a326077b8f15f
Nikki24n <109665873+Nikki24n@users.noreply.github.com>
2024-02-21 16:22:18 +0530

    final commit

--- app/src/main/java/com/example/screens/explorescreen.kt
+++ app/src/main/java/com/example/screens/explorescreen.kt
@@ -2,19 +2,20 @@ package com.example.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.tabs.TabLayout
 
 class explorescreen : AppCompatActivity() {
 
     private lateinit var bottomNavigationBar: BottomNavigationView
     private lateinit var bottomNavigationBar1: BottomNavigationView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explorescreen)
 
         bottomNavigationBar = findViewById(R.id.navigation_bar)
-        bottomNavigationBar1 = findViewById(R.id.navigation_bar1)
+
         bottomNavigationBar.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.item1 -> {
@@ -46,26 +47,7 @@ class explorescreen : AppCompatActivity() {
                 else -> false
             }
         }
-        bottomNavigationBar1.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.item1 -> {
-                    // Handle item1 reselection
-                    true
-                }
 
-                R.id.item2 -> {
-                    // Handle item2 reselection
-                    true
-                }
-
-                R.id.item3 -> {
-                    // Handle item3 reselection
-                    true
-                }
-
-                else -> false
-            }
-        }
 
     }
 }
